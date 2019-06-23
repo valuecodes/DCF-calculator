a26c585e7b0054644cb99e67e471ed37af40e9c2
Juha Kangas <juhakangas5@gmail.com>
2019-06-23 17:11:11 +0300

    Added table

--- main.js
+++ main.js
@@ -2,6 +2,13 @@
 let cashFlow = document.getElementById('cashFlow');
 let shortTerm = document.getElementById('shortTerm').value;
 let discountRate = document.getElementById('discountRate').value/100; //To decimals
+let growthRate = document.getElementById('growthRate').value/100+1; //To decimals
+let perpetuity = document.getElementById('perpetuity').value/100;
+// let WACC = 0.0847; //Optional terminal value https://www.youtube.com/watch?v=77ivvN2Uk28
+
+console.log('perpetuity '+perpetuity);
+
+console.log(growthRate);
 
 console.log(discountRate);
 // Output
@@ -10,14 +17,81 @@ let intrinsicValue = document.getElementById('intrinsicVal');
 // Calculation when button pressed
 function refresh(){
     let cash=cashFlow.value;
-    let total=0;
-    let oneYear=0;
+    let total=0; // Total cashflows
+    let oneYear=0; //One year cashflows
+    
+    // Data for the table 
+    let FCF=[];//Freecash flows
+    let DF=[];
+    let DFvalue=1;
+    let DFCF=[];
+
     for(var i=1;i<=shortTerm;i++){
         oneYear=cash/Math.pow((1+discountRate),i);
+        cash=cash*growthRate;
+        FCF.push(Math.round(cash*100)/100);
+        DFvalue=DFvalue*growthRate;
+        DF.push((Math.round(DFvalue*100)/100));
+        console.log('cash'+cash);
+        oneYear*=growthRate;
+        DFCF.push(Math.round(oneYear*100)/100);
         console.log(oneYear);
         total+=oneYear;
     }
 
+    console.log('final year '+oneYear);
+
+    // Perpetuity cashflows after 10 year
+    // let terminal=oneYear*(1+perpetuity)/(WACC-perpetuity); //Optional terminal value
+    let terminal=oneYear*(1+perpetuity)/(discountRate-perpetuity);
+    console.log('terminal '+terminal);
     total=Math.round(total * 100) / 100;
-    intrinsicValue.innerHTML=total;
+    intrinsicValue.innerHTML=total+' B';
+
+    // Creates table
+    createTable(FCF,DF,DFCF,total,terminal);
 }
+
+function createTable(dataFCF,dataDF,dataDFCF,total,terminal){
+    // Create years
+    for(var i=1;i<=10;i++){
+        var block= document.createElement('div');
+        block.className='block';
+        block.innerHTML=i;
+        document.getElementsByClassName('data')[0].appendChild(block);
+    }
+
+    // Create FCF
+    for(var i=1;i<=10;i++){
+        var block= document.createElement('div');
+        block.className='block';
+        block.innerHTML=dataFCF[i-1];
+        document.getElementsByClassName('data')[0].appendChild(block);
+    }
+
+    // Create DF
+    for(var i=1;i<=10;i++){
+        var block= document.createElement('div');
+        block.className='block';
+        block.innerHTML=dataDF[i-1];
+        document.getElementsByClassName('data')[0].appendChild(block);
+    }
+
+    // Create DFCF
+    for(var i=1;i<=10;i++){
+        var block= document.createElement('div');
+        block.className='block';
+        block.innerHTML=dataDFCF[i-1];
+        document.getElementsByClassName('data')[0].appendChild(block);
+    }
+
+    // Create Sum of DFCF and Terminal
+
+    for(var i=0;i<4;i++){
+        var sumOfDFCF = document.createElement('div');
+        sumOfDFCF.className = 'blockSum';
+        document.getElementsByClassName('results')[0].appendChild(sumOfDFCF);
+    }
+    
+}
+
